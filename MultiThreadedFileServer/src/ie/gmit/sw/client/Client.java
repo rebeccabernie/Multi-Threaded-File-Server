d3eb416d3c3837ee5410ae38cd03dfd93a7d7819
rebeccabernie <rebecca.kane@outlook.com>
2017-01-06 14:01:21 +0000

    Sets up a connection now, code in general rearranged a bit

--- MultiThreadedFileServer/src/ie/gmit/sw/client/Client.java
+++ MultiThreadedFileServer/src/ie/gmit/sw/client/Client.java
@@ -1,81 +1,83 @@
-//G00320698 - Rebecca Kane
+// G00320698 - Rebecca Kane
 package ie.gmit.sw.client; // keeping close to project spec...
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
 import java.util.logging.*;
 
 public class Client {
-
-	private static Socket socket;
-	private static String fileName;
-	static Scanner console = new Scanner(System.in);
+	// Request code from WebClient - Lecture Notes / Practical Resources on moodle
+	// In theory would be text file with list of file names?
+	// final because request won't change
+	private final String request = "GET /characters.txt HTTP/1.1\n\n"; 
 	
-	public static void main(String[] args) throws Throwable{
-		socket = new Socket ("localhost", 7777);
-		int choice = 0;
-		
+	private static Socket socket;
+	private static final int PORT = 7777; // Specified port in spec
+	private static String fileName; // File for downloading
+	static Scanner console = new Scanner(System.in);
+	private static int choice; // User's choice on menu
+	
+	public static void main(String[] args) throws Throwable {
+
+		Client client = new Client();
+		client.run();		
+
+		// Menu 
 		System.out.println("1: Connect to Server");
 		System.out.println("2: Print File Listing");
 		System.out.println("3: Download File");
 		System.out.println("4: Quit");
 		choice = console.nextInt();
+
+	} // end main
+	
+	// Empty Constructor
+	public Client() {
+		
+	}
+	
+	public void run() throws Throwable {
 		
 		// While loop specified in project spec
 		while(choice != 4){
 			
 			if(choice == 1){
 				// Connect to server
-				// ...
-			}
+				try { 
+					socket = new Socket("localhost", PORT);
+					System.out.println("Socket started on " + PORT); // Just confirmation to the user
+						
+				} catch (IOException e) { // In case something goes wrong 
+					System.out.println("Error - " + e.getMessage());
+				}
+			} // End choice 1
+			
 			else if(choice == 2){
-				// From WebClient - Lecture Notes / Practical Resources on moodle, just testing
-				// In theory would be text file with list of file names?
-				final String request = "GET /files.txt HTTP/1.1\n\n"; //The message to send to the server
-			}
+				// List of Files available to download
+				System.out.println("Files Available for Download:");
+				//list of files here...
+			} // End choice 2
+			
 			else if(choice == 3){
 				//downloadFile(); // run download file method...
-			}
+			} // End choice 3
+			
 			else if(choice == 4){
 				// End program
+				socket.close(); // Close connection, good practice
+				System.out.println("Connection closed...");
+				
 				System.out.println("Program terminated.");
-			}
+			} // End choice 4
+			
 			else{ // just in case user tries an invalid option
 				System.out.println("Please enter a valid option...");
-				choice = console.nextInt();
+				choice = console.nextInt(); // Allow user to enter an option after error displayed
 			}
-		}// end while
-
-	} // end main
+			
+		} // End while
 	
-	
-	// Code adapted from -
-	// http://codereview.stackexchange.com/questions/20961/java-multi-thread-file-server-and-client	
-	public void downloadFile() {
-		try {
-            int bytesRead;
-            InputStream in = socket.getInputStream();
-
-            DataInputStream clientData = new DataInputStream(in);
-
-            fileName = clientData.readUTF();
-            OutputStream output = new FileOutputStream(("received_from_server_" + fileName));
-            long size = clientData.readLong();
-            byte[] buffer = new byte[1024];
-            //Reading file as long as amount read != 0?
-            while (size > 0 && (bytesRead = clientData.read(buffer, 0, (int) Math.min(buffer.length, size))) != -1) {
-                output.write(buffer, 0, bytesRead);
-                size -= bytesRead; 
-            }
-
-            output.close();
-            in.close();
-
-            System.out.println("File "+fileName+" received from Server.");
-        } catch (IOException ex) {
-            Logger.getLogger(CLIENTConnection.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-	
-} // end Client
+	} // End run()
+		
+} // End Client class
